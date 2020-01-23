a9ecc14dd5b7de38b018b1b93a0e3561d6243030
Ian Yen <you@example.com>
2020-01-23 16:09:27 -0500

    Updates

---
+++ precache-manifest.671d23259931aa99c41bcb6a26d2e220.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed19a98c15c21e109e3e89fa96120bf4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "68823ea95dc070a3d6bc",
+    "url": "/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "a2dccd1428d580aea3db",
+    "url": "/static/css/main.5e3cfcc5.chunk.css"
+  },
+  {
+    "revision": "68823ea95dc070a3d6bc",
+    "url": "/static/js/2.d5974656.chunk.js"
+  },
+  {
+    "revision": "a2dccd1428d580aea3db",
+    "url": "/static/js/main.539ac489.chunk.js"
+  },
+  {
+    "revision": "9f7ae9d28d7655e5f1cb",
+    "url": "/static/js/runtime-main.0d961bae.js"
+  },
+  {
+    "revision": "015550382f1b425379556839f172bb8b",
+    "url": "/static/media/browncover.01555038.png"
+  },
+  {
+    "revision": "567d7845c9ed7ae8dd249de0de483364",
+    "url": "/static/media/ectomorph.567d7845.png"
+  },
+  {
+    "revision": "39e9b223de785d2e8a027938e3dea0fe",
+    "url": "/static/media/endomorph.39e9b223.png"
+  },
+  {
+    "revision": "cb68b50792e0e5354ab9f4733132294c",
+    "url": "/static/media/mesomorph.cb68b507.png"
+  }
+]);
