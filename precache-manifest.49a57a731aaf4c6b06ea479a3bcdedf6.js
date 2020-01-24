a604a1975a7113cba835e9d9d80b524d35c075e8
Ian Yen <you@example.com>
2020-01-24 16:51:02 -0500

    Updates

---
+++ precache-manifest.49a57a731aaf4c6b06ea479a3bcdedf6.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8cef3ce32dd02124450e3ccf1d61093",
+    "url": "/react-project/index.html"
+  },
+  {
+    "revision": "c8ba7a5e8ff607f9cf4e",
+    "url": "/react-project/static/css/2.83ace7dd.chunk.css"
+  },
+  {
+    "revision": "da6af365c46d726825b6",
+    "url": "/react-project/static/css/main.e0e2ae2a.chunk.css"
+  },
+  {
+    "revision": "c8ba7a5e8ff607f9cf4e",
+    "url": "/react-project/static/js/2.76db5bb3.chunk.js"
+  },
+  {
+    "revision": "da6af365c46d726825b6",
+    "url": "/react-project/static/js/main.150c5db2.chunk.js"
+  },
+  {
+    "revision": "aa1a030a98406532feee",
+    "url": "/react-project/static/js/runtime-main.f260eff0.js"
+  },
+  {
+    "revision": "015550382f1b425379556839f172bb8b",
+    "url": "/react-project/static/media/browncover.01555038.png"
+  },
+  {
+    "revision": "567d7845c9ed7ae8dd249de0de483364",
+    "url": "/react-project/static/media/ectomorph.567d7845.png"
+  },
+  {
+    "revision": "39e9b223de785d2e8a027938e3dea0fe",
+    "url": "/react-project/static/media/endomorph.39e9b223.png"
+  },
+  {
+    "revision": "cb68b50792e0e5354ab9f4733132294c",
+    "url": "/react-project/static/media/mesomorph.cb68b507.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0ca53518c3ae97f51b131dff6f7215d5.js"
+  "/react-project/precache-manifest.49a57a731aaf4c6b06ea479a3bcdedf6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-project/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
